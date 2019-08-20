089fc16360212317923af58ab46c772c0d4179f8
Karina Nowicka <knowicka@gmail.com>
2019-08-20 21:24:31 +0200

    Updates

---
+++ precache-manifest.5c5961ff44669c6499bde1694f32308c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce395ef87b14b1460ae006aed32e0930",
+    "url": "/scrabble-workout/index.html"
+  },
+  {
+    "revision": "01f719f8083278f0449a",
+    "url": "/scrabble-workout/static/css/main.ce812aca.chunk.css"
+  },
+  {
+    "revision": "aab4d9d7444625f128c2",
+    "url": "/scrabble-workout/static/js/2.60612b09.chunk.js"
+  },
+  {
+    "revision": "01f719f8083278f0449a",
+    "url": "/scrabble-workout/static/js/main.8d16e112.chunk.js"
+  },
+  {
+    "revision": "e6f3ecc0591c07c20123",
+    "url": "/scrabble-workout/static/js/runtime~main.3fa9b69d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/scrabble-workout/static/media/logo.5d5d9eef.svg"
+  }
+]);
