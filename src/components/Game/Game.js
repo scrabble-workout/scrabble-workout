77e24994d77af98f13cce1af58deeecf8a5f334d
Karina Nowicka <knowicka@gmail.com>
2019-08-24 18:42:36 +0200

    Add cells and letters to Game screen

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import classes from './Game.scss';
 
-const Game = () => (
-    <main className={classes.Game}>
-        <h2>Game Screen</h2>
-    </main>
-);
+const Game = () => {
+    const letterClasses = [
+        classes.Letter,
+        classes.Inactive,
+    ];
+
+    return (
+        <main className={classes.Game}>
+            <section className={classes.Cells}>
+                <div className={classes.Cell}>a</div>
+                <div className={classes.Cell}>t</div>
+                <div className={classes.Cell}>l</div>
+                <div className={classes.Cell}></div>
+                <div className={classes.Cell}></div>
+                <div className={classes.Cell}></div>
+                <div className={classes.Cell}></div>
+            </section>
+            <section className={classes.Letters}>
+                <div className={classes.LettersRow}>
+                    <div className={letterClasses.join(' ')}>a</div>
+                    <div className={classes.Letter}>s</div>
+                </div>
+                <div className={classes.LettersRow}>
+                    <div className={letterClasses.join(' ')}>t</div>
+                    <div className={classes.Letter}>r</div>
+                    <div className={classes.Letter}>k</div>
+                </div>
+                <div className={classes.LettersRow}>
+                    <div className={letterClasses.join(' ')}>l</div>
+                    <div className={classes.Letter}>q</div>
+                </div>
+            </section>
+        </main>
+    );
+};
 
 export { Game };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,8 +5,12 @@ import { Button } from '../Button/Button';
 
 const Home = () => (
     <main className={classes.Home}>
-        <h2 className={classes.Header}>Home Screen</h2>
-        <Button />
+        <section className={classes.Hero}>
+            <h2 className={classes.Header}>Home Screen</h2>
+        </section>
+        <section className={classes.BtnSection}>
+            <Button />
+        </section>
     </main>
 );
 
