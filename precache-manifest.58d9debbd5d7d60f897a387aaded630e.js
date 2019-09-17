7d1764e6fdf5e2a9904d6419794fb32d10bb7751
CircleCI <builds@circleci.com>
2019-09-17 11:14:03 +0000

    [ci skip] Updates

--- precache-manifest.58d9debbd5d7d60f897a387aaded630e.js
+++ precache-manifest.58d9debbd5d7d60f897a387aaded630e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c5cfc3dcf3fa1e3110d0bcf7c0f3bdec",
+    "revision": "8bbd1d06e5fbd197b62cbb8b69b2ea13",
     "url": "/scrabble-workout/index.html"
   },
   {
-    "revision": "956cf2190e91499dd939",
+    "revision": "21b9c1349f1e6e9ef5a4",
     "url": "/scrabble-workout/static/css/2.d076884b.chunk.css"
   },
   {
-    "revision": "c658115a0ad60fca4f31",
+    "revision": "fe04e6e00f2d713d34bb",
     "url": "/scrabble-workout/static/css/main.9b765068.chunk.css"
   },
   {
-    "revision": "956cf2190e91499dd939",
-    "url": "/scrabble-workout/static/js/2.cfb41206.chunk.js"
+    "revision": "21b9c1349f1e6e9ef5a4",
+    "url": "/scrabble-workout/static/js/2.0ebf21c3.chunk.js"
   },
   {
-    "revision": "c658115a0ad60fca4f31",
-    "url": "/scrabble-workout/static/js/main.8c4355f3.chunk.js"
+    "revision": "fe04e6e00f2d713d34bb",
+    "url": "/scrabble-workout/static/js/main.1694df7a.chunk.js"
   },
   {
     "revision": "e6f3ecc0591c07c20123",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/scrabble-workout/precache-manifest.a7dd9322b49f53db5a30a8bee6c2d244.js"
+  "/scrabble-workout/precache-manifest.58d9debbd5d7d60f897a387aaded630e.js"
 );
 
 self.addEventListener('message', (event) => {
