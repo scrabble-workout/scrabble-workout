46cc6ebf8ef2ed57306fa38d3fbaf49da5fd2c0b
Karina Nowicka <knowicka@gmail.com>
2019-09-28 16:05:08 +0200

    Add throwing error and additional tests

--- src/helpers/format-duration.js
+++ src/helpers/format-duration.js
@@ -1,5 +1,9 @@
 const formatDuration = (duration) => {
-    const minutes = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60)).toString();
+    if (!Number.isInteger(duration) || duration < 0) {
+        throw new Error('Error in formatDuration function: parameter is not a non-negative integer. Please provide a proper parameter');
+    }
+
+    const minutes = Math.floor(duration / (1000 * 60)).toString();
     const seconds = Math.floor((duration % (1000 * 60)) / 1000).toString().padStart(2, '0');
     return `${minutes}:${seconds}`;
 };
