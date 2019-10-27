c0b7b9823043a5d0d64d54cf8f4ca768bbcb7c70
Karina Nowicka <knowicka@gmail.com>
2019-10-27 10:40:26 +0100

    Add comment to matchMedia

--- src/config/config.js
+++ src/config/config.js
@@ -2,6 +2,7 @@ export const WORD_LENGTH = 7;
 export const DURATION = 12000000;
 export const INTERVAL = 1000;
 
+/* a piece of magic due to the fact that matchMedia isn't implemented in JSDOM (used by Jest) yet */
 window.matchMedia = window.matchMedia || (() => (
     {
         matches: false,
